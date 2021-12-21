8a03284f230d81474e53c33cf17306483fdfea9e
mstfrs <mstfrs68@gmail.com>
2021-12-21 22:41:09 +0300

    RandomUser project finished

--- project/004-random-user/src/App.js
+++ project/004-random-user/src/App.js
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from "react";
 import email from "./assets/email.svg";
 import phone from "./assets/phone.svg";
 import location from "./assets/location.svg";
+import "./App.css";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState("");
   const getUser = () => {
     axios.get(`https://randomuser.me/api/`).then((response) => {
       console.log(response);
@@ -18,36 +19,48 @@ const App = () => {
   useEffect(() => getUser(), []);
   return (
     <div>
-      {users.results.map((user) => (
+      {users ? (
         <div className="container">
           <div className="grid-container">
             <div className="grid-item">
-              <img src={user.picture.medium} alt="" id="userimg" />
+              <img src={users.results[0].picture.medium} alt="" id="userimg" />
             </div>
 
-            <div className="grid-item">name:</div>
+            <div className="grid-item">
+              <h3>
+                {users.results[0].name.title} {users.results[0].name.first}
+                {users.results[0].name.last}
+              </h3>
+            </div>
 
             <div className="grid-item">
               <img src={email} alt="" />
             </div>
-            <div className="grid-item">{user.email} </div>
+            <div className="grid-item">{users.results[0].email} </div>
 
             <div className="grid-item">
               <img src={phone} alt="" />
             </div>
-            <div className="grid-item">{user.phone}</div>
+            <div className="grid-item">{users.results[0].phone}</div>
 
             <div className="grid-item">
               <img src={location} alt="" />
             </div>
-            <div className="grid-item">{user.location.city} </div>
-
-            <div className="grid-item">Age: {user.dob.age} </div>
+            <div className="grid-item">{users.results[0].location.city} </div>
+          </div>
+          <div className="grid-item">Age: {users.results[0].dob.age} </div>
+          <div className="grid-item">
+            Register Date: {users.results[0].registered.date}
           </div>
 
-          <button className="btn-primary"></button>
+          <button className="btn-primary" onClick={getUser}>
+            Random
+          </button>
         </div>
-      ))}
+      ) : (
+        <div>Loading....</div>
+      )}
+      ;
     </div>
   );
 };
